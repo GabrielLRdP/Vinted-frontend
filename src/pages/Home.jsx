a90b3a0c7eb41999f5ead1d3c9ba87213afe46c4
Gabriel <gabriel.le.roy06@gmail.com>
2024-04-29 16:52:27 +0200

    added ripped effect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -50,7 +50,7 @@ const Home = ({ search, values, priceAscending }) => {
     <main className="home">
       <section className="presentation">
         <img className="main-image" src={mainImg} alt="image de présentation" />
-        <img className="effet" src={rippedEffect} alt="" />
+        <div className="effet" />
         <div className="container">
           <div className="call-to-action">
             <h1>Prêt à faire du tri dans vos placards ?</h1>
